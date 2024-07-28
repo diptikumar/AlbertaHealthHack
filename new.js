455efc8d1a49eab617daecc24818f13832e3eec5
zainab_majid <zainabmajid2408@gmail.com>
2024-07-27 21:16:56 -0600

    working on functions for skill notes

--- new.js
+++ new.js
@@ -16,3 +16,33 @@ function verifyEmail() {
 }
 
 //code for the skills progress section
+
+//gets the skills from cloud storage: needed a bit of help from chat gpt
+function getSkills(){
+
+}
+
+//takes in array of notes and saves them to local storage
+function saveSkills(skills){
+
+}
+
+//creates a new html element that represents a new skill
+function createNewSkill(id, content){
+
+}
+
+//allows us to add a new skill note to html and local storage
+function addSkill() {
+
+}
+
+//updates skill note with new content
+function updateSkill(id, newContent){
+
+}
+
+//deletes the note with a given id and html element
+function deleteSkill(id, element){
+
+}
