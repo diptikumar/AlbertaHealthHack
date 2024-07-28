c9712cc3040009af2fa5edf47a793b18e72058be
blueracoon010 <saira13fatima@gmail.com>
2024-07-27 21:00:39 -0600

    Wrote the code for initiang Flask

---
+++ Backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from models import db
+from routes.user_routes import user_bp
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+db.init_app(app)
+
+app.register_blueprint(user_bp, url_prefix='/api/users')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/search')
+def search_page():
+    return app.send_static_file('search.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
