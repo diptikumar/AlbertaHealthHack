5e5f56aad7b0e38e93e34b7b2e2df50d58d57816
blueracoon010 <saira13fatima@gmail.com>
2024-07-28 04:01:25 -0600

    changed the backend

--- Backend/init_db.py
+++
@@ -1,9 +0,0 @@
-
-#  This Script initializes  the database by creating tables 
-# is defined in the models 
-
-from app import app
-from models import db
-
-with app.app_context():
-    db.create_all()
--- Backend/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file can be used for package-level documentation or imports.
-# Currently, it is empty.
-
-# This file is intentionally left empty.
