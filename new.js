52c59a7762bcc78ee8796a094a03d1a89c7325a1
Dipti <dipti.kumar@ucalgary.ca>
2024-07-27 21:49:23 -0600

    volunteer verification done

--- new.js
+++ new.js
@@ -1,15 +1,17 @@
+let verifiedUsers = [];
+
 function back() {
     window.history.back();
 }
 
 function verifyEmail() {
-    const email = document.getElementById("email").ariaValueMax;
+    const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     if (email === 'dipti1kumar@gmail.com') {
-        window.location.href = 'volunteer/volunteerVerified.html';
+        window.location.href = 'volunteerVerified.html';
     } else {
-       window.location.href = 'volunteer/volunteerNotVerified.html';
+        window.location.href = 'volunteerNotVerified.html';
     }
 
 }
