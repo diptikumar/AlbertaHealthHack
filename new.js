95348c82a729c660d4c9dd07f5046d47eed53b13
Dipti <dipti.kumar@ucalgary.ca>
2024-07-27 21:04:47 -0600

    volunteer logins

--- new.js
+++ new.js
@@ -3,3 +3,15 @@
 function back() {
     window.history.back();
 }
+
+function verifyEmail() {
+    const email = document.getElementById("email").ariaValueMax;
+    const password = document.getElementById("password").value;
+
+    if (email === 'dipti1kumar@gmail.com') {
+        window.location.href = 'volunteer/volunteerVerified.html';
+    } else {
+       window.location.href = 'volunteer/volunteerNotVerified.html';
+    }
+
+}
